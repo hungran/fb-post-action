12b70facbf6257ad3fbb9cdea8c4268139012913
hungran <26101787+hungran@users.noreply.github.com>
2023-05-31 16:08:52 +0700

    feat: init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main.go
+
+go 1.19
+
+require github.com/google/go-github/v52 v52.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Post struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	accessToken := os.Getenv("FACEBOOK_ACCESS_TOKEN")
+	pageID := os.Getenv("FACEBOOK_PAGE_ID")
+	url := fmt.Sprintf("https://graph.facebook.com/v13.0/%s/feed", pageID)
+
+	post := Post{
+		Message: "Hello, Facebook! This is my first post using Go!",
+	}
+
+	jsonData, err := json.Marshal(post)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating HTTP request:", err)
+		return
+	}
+
+	req.URL.RawQuery = fmt.Sprintf("access_token=%s", accessToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending HTTP request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Post successfully published on Facebook!")
+}
