b4aaa7b60d5622ba20631990f3916db88fd035c1
hungran <26101787+hungran@users.noreply.github.com>
2023-05-31 16:20:33 +0700

    typo

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	url := fmt.Sprintf("https://graph.facebook.com/v13.0/%s/feed", pageID)
 
 	post := Post{
-		Message: os.Getenv("FACEBOOK_POST_MESSAGE")),
+		Message: os.Getenv("FACEBOOK_POST_MESSAGE"),
 	}
 
 	jsonData, err := json.Marshal(post)
